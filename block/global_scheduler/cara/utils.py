cccc7e5956a2937e01618324a53e5519f2a01ff6
Wei Da <weida@Weis-MacBook-Air-4.local>
2025-12-15 02:39:47 +0000

    fix the set_ulimits with new imports

--- block/global_scheduler/cara/utils.py
+++ block/global_scheduler/cara/utils.py
@@ -1,3 +1,5 @@
+import sys
+
 STOP_WORD_MAPS = {
     "Qwen": ["<|im_start|>", "<|im_end|>"]
 }
@@ -5,7 +7,7 @@ STOP_WORD_MAPS = {
 
 def set_ulimit(target_soft_limit: int = 65535):
     if sys.platform.startswith("win"):
-        logger.info("Windows detected, skipping ulimit adjustment.")
+        print("Windows detected, skipping ulimit adjustment.")
         return
 
     import resource
@@ -17,7 +19,7 @@ def set_ulimit(target_soft_limit: int = 65535):
         try:
             resource.setrlimit(resource_type, (target_soft_limit, current_hard))
         except ValueError as e:
-            logger.warning(
+            print(
                 "Found ulimit of %s and failed to automatically increase "
                 "with error %s. This can cause fd limit errors like "
                 "`OSError: [Errno 24] Too many open files`. Consider "
