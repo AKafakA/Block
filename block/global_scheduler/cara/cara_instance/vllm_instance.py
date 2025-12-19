8c7b3519b2eb663925feeed769f2436a9f8be07e
Wei Da <daweiuestc@gmail.com>
2025-12-19 18:38:23 +0000

    fix the vllm payload for chat api

--- block/global_scheduler/cara/cara_instance/vllm_instance.py
+++ block/global_scheduler/cara/cara_instance/vllm_instance.py
@@ -104,8 +104,7 @@ class VllmInstance(Instance):
                 "messages": payload["messages"],
                 "temperature": 0.0,
                 "repetition_penalty": payload.get("repetition_penalty", 1.0),
-                "max_tokens": payload["max_tokens"],
-                "logprobs": None,
+                "max_completion_tokens": payload["max_tokens"],
                 "stream": True,
                 "stream_options": {
                     "include_usage": True,
@@ -119,7 +118,8 @@ class VllmInstance(Instance):
                 "prompt": payload["prompt"],
                 "temperature": 0.0,
                 "repetition_penalty": payload.get("repetition_penalty", 1.0),
-                "max_completion_tokens": payload["max_tokens"],
+                "max_tokens": payload["max_tokens"],
+                "logprobs": None,
                 "stream": True,
                 "stream_options": {
                     "include_usage": True,
